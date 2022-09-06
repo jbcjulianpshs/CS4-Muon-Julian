c71faec51618cec28603bea1c4f542b1b237d128
jbcjulianpshs <100216963+jbcjulianpshs@users.noreply.github.com>
2022-09-06 08:55:56 +0800

    Add files via upload

---
+++ q1_excercise1/Q1_Excercise1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_excercise1;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Excercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name = "John Brandon C. Julian";
+        System.out.println("Hi, my name is " + name + ", nice to meet you.");
+        
+        int num = 3;
+        double decimal = 4.5;
+        char letter = 'a';
+        String word = "word";
+        
+        num %= 2;
+        System.out.println(num);
+        System.out.println();
+    }
+    
+}
