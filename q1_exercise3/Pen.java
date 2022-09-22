aa69491298da8efbee477d5c5353d0eb49819240
jbcjulianpshs <100216963+jbcjulianpshs@users.noreply.github.com>
2022-09-22 11:26:11 +0800

    Add files via upload

---
+++ q1_exercise3/Pen.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise3;
+
+/**
+ *
+ * @author MUON
+ */
+public class Pen {
+    String owner;
+    String color = "Black"; 
+    String brand = "HBW";    
+    
+    public Pen(String n){
+    this.owner = n;
+    }
+}
