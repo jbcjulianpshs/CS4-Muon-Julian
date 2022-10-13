5b2d6430b7e9834ff51bab9a801dd00d9af280f4
jbcjulianpshs <100216963+jbcjulianpshs@users.noreply.github.com>
2022-10-13 21:14:57 +0800

    Update Store.java

--- q1_exercise5/Store.java
+++ q1_exercise5/Store.java
@@ -12,9 +12,9 @@ public class Store {
     // Initialize name to parameter and earnings to zero
     // Initialize itemList as a new ArrayList
     // add 'this' store to storeList
-    this.name = name;
-    ArrayList<Item> itemList = new ArrayList<>();
-    storeList.add(this);
+        this.name = name;
+        this.itemList = new ArrayList<>();
+        storeList.add(this);
   }
   
   public String getName(){
@@ -28,9 +28,11 @@ public class Store {
         if(index < itemList.size()){
             earnings += itemList.get(index).getCost();
             System.out.println(itemList.get(index).getName() + " has been sold for " + itemList.get(index).getCost());
+            System.out.println("---------");
             itemList.remove(index);
         }else{
-            System.out.println("There are only " + itemList.size() + " items in the store");
+            System.out.println("There are only " + itemList.size() + " items in " + getName());
+            System.out.println("---------");
         }
     // get Item at index from itemList and add its cost to earnings
     // print statement indicating the sale
@@ -50,9 +52,11 @@ public class Store {
         if(found){
             earnings += itemList.get(index).getCost();
             System.out.println(itemList.get(index).getName() + " has been sold for " + itemList.get(index).getCost());
+            System.out.println("---------");
             itemList.remove(index);
         }else{
-            System.out.println("Sorry, this item is not in the store");
+            System.out.println("Sorry, this item is not in " + getName());
+            System.out.println("---------");
         }
         
     // get Item from itemList and add its cost to earnings
@@ -63,9 +67,11 @@ public class Store {
         if(itemList.contains(i)){
             earnings += i.getCost();
             System.out.println(i.getName() + " has been sold for " + i.getCost());
+            System.out.println("---------");
             itemList.remove(i);
         }else{
-            System.out.println("Sorry, this item is not in the store");
+            System.out.println("Sorry, this item is not in " + getName());
+            System.out.println("---------");
         }
     
     
@@ -78,20 +84,40 @@ public class Store {
   }
   public void filterType(String type){
     // loop over itemList and print all items with the specified type
+        System.out.println("Items in " + getName() + " with type " + type + ":");
         for(Item i : itemList){
             if(i.getType().equalsIgnoreCase(type)){
-                System.out.println(i.getType());
+                System.out.println(i.getName());
             }
         }
+        System.out.println("---------");
   }
   public void filterCheap(double maxCost){
     // loop over itemList and print all items with a cost lower than or equal to the specified value
+        System.out.println("Items in " + getName() + " with cost lower than or equal to " + maxCost + ":");
+        for(Item i : itemList){
+            if(i.getCost() <= maxCost){
+                System.out.println(i.getName());
+            }
+        }
+        System.out.println("---------");
   }
   public void filterExpensive(double minCost){
     // loop over itemList and print all items with a cost higher than or equal to the specified value
+        System.out.println("Items in " + getName() + " with cost higher than or equal to " + minCost + ":");
+        for(Item i : itemList){
+            if(i.getCost() >= minCost){
+                System.out.println(i.getName());
+            }
+        }
+        System.out.println("---------");
   }
   public static void printStats(){
     // loop over storeList and print the name and the earnings'Store.java'
-
+        for(Store s : storeList){
+            System.out.println("Store Name: " + s.getName());
+            System.out.println("Store Earnings: " + s.getEarnings());
+            System.out.println("---------");
+        }
   }
 }
