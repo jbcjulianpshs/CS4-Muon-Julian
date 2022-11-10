ce3b9c8a7224a393eb2d547beed708e2efaf871e
jbcjulianpshs <100216963+jbcjulianpshs@users.noreply.github.com>
2022-11-10 22:01:17 +0800

    Update Q2_exercise6.java

--- q2_exercise6/Q2_exercise6.java
+++ q2_exercise6/Q2_exercise6.java
@@ -16,6 +16,25 @@ public class Q2_exercise6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        FireType salamander = new FireType("Salamander", 50, 10);
+        WaterType undine = new WaterType("Undine", 50, 10);
+        GrassType nymph = new GrassType("Nymph", 60, 8);
+        
+        Monster.deathDuel(nymph, salamander);
+        
+        nymph.restoreHealth();
+        salamander.restoreHealth();
+        
+        Monster.deathDuel(salamander, undine);
+        
+        salamander.restoreHealth();
+        undine.restoreHealth();
+        
+        Monster.deathDuel(undine, nymph);
+        
+        undine.restoreHealth();
+        nymph.restoreHealth();
     }
     
 }
